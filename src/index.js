1f94cee885943b0e8dca34c18a95deb0fd58141e
tdd3vlp <tdd3vlp@gmail.com>
2022-08-11 14:00:18 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrayed = str.split('');
+  let dupl;
+
+  function trim(str) {
+    dupl = str.slice();
+
+    bracketsConfig.forEach((el) => {
+      for (let i = 0; i < dupl.length; i++) {
+        if (dupl[i] === el[0] && dupl[i + 1] === el[1]) dupl.splice(i, 2);
+      }
+    });
+
+    if (dupl.length === 0) return true;
+    if (JSON.stringify(dupl) === JSON.stringify(str)) return false;
+
+    return trim(dupl);
+  }
+
+  let result = trim(arrayed);
+  return result;
+};
